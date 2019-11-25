84b65e63efd15341ff80a2bb31d256b99d517c6b
Thiago Elias <thiagopters@gmail.com>
2019-11-25 20:21:43 -0300

    Refactoring

--- assets.py
+++ assets.py
@@ -23,30 +23,12 @@ class assets():
 
     def getMan(self):
         obj = Object()
+        obj.right = []
+        obj.left = []
 
-        obj.right = [
-            self.pygame.image.load(self.chImages + 'R1.png'),
-            self.pygame.image.load(self.chImages + 'R2.png'),
-            self.pygame.image.load(self.chImages + 'R3.png'),
-            self.pygame.image.load(self.chImages + 'R4.png'),
-            self.pygame.image.load(self.chImages + 'R5.png'),
-            self.pygame.image.load(self.chImages + 'R6.png'),
-            self.pygame.image.load(self.chImages + 'R7.png'),
-            self.pygame.image.load(self.chImages + 'R8.png'),
-            self.pygame.image.load(self.chImages + 'R9.png')
-        ]
-
-        obj.left = [
-            self.pygame.image.load(self.chImages + 'L1.png'),
-            self.pygame.image.load(self.chImages + 'L2.png'),
-            self.pygame.image.load(self.chImages + 'L3.png'),
-            self.pygame.image.load(self.chImages + 'L4.png'),
-            self.pygame.image.load(self.chImages + 'L5.png'),
-            self.pygame.image.load(self.chImages + 'L6.png'),
-            self.pygame.image.load(self.chImages + 'L7.png'),
-            self.pygame.image.load(self.chImages + 'L8.png'),
-            self.pygame.image.load(self.chImages + 'L9.png')
-        ]
+        for idx in range(9):
+            obj.right.append(self.pygame.image.load("{}R{}.png".format(self.chImages, idx + 1)))
+            obj.left.append(self.pygame.image.load("{}L{}.png".format(self.chImages, idx + 1)))
 
         obj.char = self.pygame.image.load(self.chImages + 'standing.png')
 
@@ -54,34 +36,12 @@ class assets():
 
     def getEnemy(self):
         obj = Object()
+        obj.right = []
+        obj.left = []
 
-        obj.right = [
-            self.pygame.image.load(self.chImages + 'R1E.png'),
-            self.pygame.image.load(self.chImages + 'R2E.png'),
-            self.pygame.image.load(self.chImages + 'R3E.png'),
-            self.pygame.image.load(self.chImages + 'R4E.png'),
-            self.pygame.image.load(self.chImages + 'R5E.png'),
-            self.pygame.image.load(self.chImages + 'R6E.png'),
-            self.pygame.image.load(self.chImages + 'R7E.png'),
-            self.pygame.image.load(self.chImages + 'R8E.png'),
-            self.pygame.image.load(self.chImages + 'R9E.png'),
-            self.pygame.image.load(self.chImages + 'R10E.png'),
-            self.pygame.image.load(self.chImages + 'R11E.png')
-        ]
-
-        obj.left = [
-            self.pygame.image.load(self.chImages + 'L1E.png'),
-            self.pygame.image.load(self.chImages + 'L2E.png'),
-            self.pygame.image.load(self.chImages + 'L3E.png'),
-            self.pygame.image.load(self.chImages + 'L4E.png'),
-            self.pygame.image.load(self.chImages + 'L5E.png'),
-            self.pygame.image.load(self.chImages + 'L6E.png'),
-            self.pygame.image.load(self.chImages + 'L7E.png'),
-            self.pygame.image.load(self.chImages + 'L8E.png'),
-            self.pygame.image.load(self.chImages + 'L9E.png'),
-            self.pygame.image.load(self.chImages + 'L10E.png'),
-            self.pygame.image.load(self.chImages + 'L11E.png')
-        ]
+        for idx in range(11):
+            obj.right.append(self.pygame.image.load("{}R{}E.png".format(self.chImages, idx + 1)))
+            obj.left.append(self.pygame.image.load("{}L{}E.png".format(self.chImages, idx + 1)))
 
         obj.char = self.pygame.image.load(self.chImages + 'standing.png')
 
